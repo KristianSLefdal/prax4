7c7819015c6b2e09e7d285ebf05aee32e79b0d8f
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-15 14:09:33 +0100

    Add files via upload

---
+++ firehouse.py
@@ -0,0 +1,16 @@
+import firefighteremployee
+
+Firefighter1 = firefighteremployee.Firefighter("Kristian","Lefdal","12345","21")
+Firefighter2 = firefighteremployee.Firefighter("Kristoffer","Haugen","23456","24")
+Firefighter3 = firefighteremployee.Firefighter("Tobias","Offerdal Roneid","34567","21")
+Firefighter4 = firefighteremployee.Firefighter("bob","derek","86745","41")
+Firefighter5 = firefighteremployee.Firefighter("dylan","steve","53564","43")
+Firefighter6 = firefighteremployee.Firefighter("bob","greg","23356","51")
+
+Firefighter1.display()
+Firefighter2.display()
+Firefighter3.display()
+Firefighter4.display()
+Firefighter5.display()
+Firefighter6.display()
+
